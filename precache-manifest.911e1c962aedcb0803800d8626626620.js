c46d616886e872957a0e93653cf6f30e97a566b5
Arimazz <bodyaprymyak@icloud.com>
2020-02-17 02:10:45 +0200

    Updates

---
+++ precache-manifest.911e1c962aedcb0803800d8626626620.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c0f0df4132500f174e31fd41f662d7e",
+    "url": "/react_counter/index.html"
+  },
+  {
+    "revision": "919348022003544d4708",
+    "url": "/react_counter/static/js/2.10212bdd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react_counter/static/js/2.10212bdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "323e9bdf9ca3a7f88883",
+    "url": "/react_counter/static/js/main.55e8fa2c.chunk.js"
+  },
+  {
+    "revision": "991db1d0e8b10247e7f5",
+    "url": "/react_counter/static/js/runtime-main.baa6acde.js"
+  }
+]);
